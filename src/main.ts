c9b4f86b503977d012526cfcf2be5cc72774fee3
EddyBer <eddybernardeau@gmail.com>
2023-06-28 14:41:27 +0200

    #mineur

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true});
   await app.listen(3000);
+  app.enableCors({
+    origin: [
+      'http://localhost',
+      'http://localhost:5173'
+    ]
+  })
 }
 bootstrap();
